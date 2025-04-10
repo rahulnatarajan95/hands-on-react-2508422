96e50c044aa0002a0614ffe3cb01ff75b77ab85f
rahulnatarajan95 <61011589+rahulnatarajan95@users.noreply.github.com>
2025-04-10 22:12:33 +0000

    component hooks

---
+++ src/components/Support.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+export default () => {
+  const [count,setCount]= useState(0);
+  return (
+    <button className="outline" onClick={()=> setCount(count + 1)}>
+      {count === 0 ? 'Click to Support' : `${count} supported`}</button>
+  )
+}
