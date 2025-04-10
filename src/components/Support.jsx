a322d62f19f523f21102ed3cd350d75f2cba851e
rahulnatarajan95 <61011589+rahulnatarajan95@users.noreply.github.com>
2025-04-10 22:28:16 +0000

    useEffect

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+const title = document.title;
 
 export default () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  useEffect(()=> {
+    count && (document.title = `${title} -- ${count}`);
+  })
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
       {count === 0 ? "Click to support": `Supported ${count} times`}
