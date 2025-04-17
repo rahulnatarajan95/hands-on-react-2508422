a2858049c46001e689560dbc1877808ed1e70916
rahulnatarajan95 <61011589+rahulnatarajan95@users.noreply.github.com>
2025-04-17 18:20:00 +0000

    create custom event trigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import React, { Component } from 'react';
-import Welcome from './components/Welcome';
-import Support from './components/Support';
-import ListCast from './components/ListCast';
+import React, { Component, useState } from "react";
+import ListCast from "./components/ListCast";
 
 function App() {
-  const name = 'StarGazers'
+  const name = "StarGazers";
+  const [memberInfo, setMemberInfo] = useState(null);
   return (
     <div className="container">
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast />
-          <Support />
+          <h1>Welcome to {name}</h1>
+          <p>
+            Members of an <b>intergalactic alliance</b>
+            <br />
+            paving the way for peace and benevolence among all species. They are
+            known for their enthusiasm for science, for their love of fun, and
+            their dedication to education.
+          </p>
+          <ListCast onChoice={(member) => setMemberInfo(member)} />
+          {memberInfo && (
+            <hgroup>
+              <img
+                src={`images/${memberInfo.slug}_tn.svg`}
+                alt={memberInfo.name}
+              />
+              <hgroup>
+                <div>{memberInfo.name}</div>
+                <div>{memberInfo.bio}</div>
+              </hgroup>
+            </hgroup>
+          )}
         </hgroup>
       </article>
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- src/components/ListCast.jsx
+++ src/components/ListCast.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 
-export default () => {
+export default ({ onChoice }) => {
   const [cast, setCast] = useState([]);
 
   async function fetchCast() {
-    const response = await fetch('cast.json');
+    const response = await fetch("cast.json");
     setCast(await response.json());
   }
 
@@ -13,19 +13,23 @@ export default () => {
   });
 
   return (
-    <div style={{
-      display: "grid",
-      gridTemplateColumns: `repeat(auto-fit, minmax(90px, 1fr))`,
-      gap: `1rem`,
-      marginBottom: '1rem'
-    }}>
-      {
-        cast.map(member => (
-          <a key={member.id} data-tooltip={member.name}>
-            <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
-          </a>
-        ))
-      }
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(auto-fit, minmax(90px, 1fr))`,
+        gap: `1rem`,
+        marginBottom: "1rem",
+      }}
+    >
+      {cast.map((member) => (
+        <a
+          onClick={() => onChoice(member)}
+          key={member.id}
+          data-tooltip={member.name}
+        >
+          <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
+        </a>
+      ))}
     </div>
-  )
-}
+  );
+};
