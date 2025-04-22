c5a77735e475a08b889e4805cf84445b39dbdb00
rahulnatarajan95 <61011589+rahulnatarajan95@users.noreply.github.com>
2025-04-22 03:26:13 +0000

    handle next and previous in modal

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,28 +1,58 @@
-export default ({ member, handleClose }) => {
+import { useState } from "react";
+
+export default ({ member, handleClose, cast }) => {
+  const [memberInfo, setMemberInfo] = useState(member);
+  const nextMember = (member) => {
+    const newId = member.id + 1;
+    const findId = newId < cast.length ? newId : 0;
+    const newMember = cast.filter((castMember) => {
+      return castMember.id === findId;
+    });
+    setMemberInfo(newMember[0]);
+  };
+  const previousMember = (member) => {
+    const newId = member.id;
+    const findId = newId === 0 ? cast.length - 1 : newId - 1;
+    const newMember = cast.filter((castMember) => {
+      return castMember.id === findId;
+    });
+    setMemberInfo(newMember[0]);
+  };
   return (
     <dialog id="modal-member" open>
       <article>
-        <a href="#close"
+        <a
+          href="#close"
           aria-label="Close"
           className="close"
           data-target="modal-member"
           onClick={handleClose}
         ></a>
         <hgroup>
-          <div style={{
-            display: "flex",
-            gap: '1rem'
-          }}>
-            <img style={{ width: '200px' }}
-              src={`images/${member.slug}.svg`}
-              alt={member.name} />
+          <div
+            style={{
+              display: "flex",
+              gap: "1rem",
+            }}
+          >
+            <img
+              style={{ width: "200px" }}
+              src={`images/${memberInfo.slug}.svg`}
+              alt={memberInfo.name}
+            />
             <hgroup>
-              <h1>{member.name}</h1>
-              <p>{member.bio}</p>
+              <h1>{memberInfo.name}</h1>
+              <p>{memberInfo.bio}</p>
             </hgroup>
           </div>
         </hgroup>
+        <footer>
+          <button onClick={() => nextMember(memberInfo)} className="secondary">
+            Next
+          </button>
+          <button onClick={() => previousMember(memberInfo)}>Previous</button>
+        </footer>
       </article>
     </dialog>
-  )
-}
+  );
+};
